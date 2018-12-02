ca6247b0d872ba6fe6cd536a11cb53ad35979d5e
Dilshanth <dilshanth10@gmail.com>
2018-12-02 11:23:43 +0530

    api automation first commit

---
+++ src/main/java/com/test/qa/utils/APIUtils.java
@@ -0,0 +1,36 @@
+package com.test.qa.utils;
+
+import io.restassured.response.Response;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class APIUtils {
+
+    public static String readBody(String path) {
+        String body = "";
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(path));
+            body = new String(encoded, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
+
+    public static void printResults(Response response){
+        System.out.println(Constants.RESPONSE_BREAKER);
+        System.out.println(Constants.RESPOINSE_CODE_TEXT + response.getStatusCode());
+        System.out.println(Constants.RESPOINSE_DATA_TEXT + response.asString());
+        System.out.println(Constants.RESPOINSE_TIME_TEXT + response.time());
+        System.out.println(Constants.RESPONSE_BREAKER);
+    }
+
+    public static String extractResponse(Response response, String key) {
+        JSONObject obj = new JSONObject(response.asString());
+        return obj.get(key).toString();
+    }
+}
